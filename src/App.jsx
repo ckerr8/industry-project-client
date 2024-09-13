23780dee3b935acf867ccaf782a095aeab7671a1
Lily Le <lmlt8180@gmail.com>
2024-09-12 22:51:25 -0400

    import hero component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Header from "../src/components/Header/Header";
 import HomePage from "../src/pages/HomePage/HomePage";
 import Footer from "../src/components/Footer/Footer";
 import "./styles/global.scss";
+import { Hero } from './components/Hero/Hero';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
+      <Hero />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
