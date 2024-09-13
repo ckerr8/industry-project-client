d84c63eeffba67dd4d3f3a7a4fa9f58b354e1f4b
Lily Le <lmlt8180@gmail.com>
2024-09-12 20:52:28 -0400

    create hero jsx element structure, add placeholder

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.scss";
+
+export function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero__container">
+                <div className="hero__overlay">
+                    Placeholder Hero
+                </div>
+            </div>
+        </section>
+    )
+};
