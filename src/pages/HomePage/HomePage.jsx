766015e420df4806d6bb7089d2c7fead0293870d
Chelsea Kerr <54897046+ckerr8@users.noreply.github.com>
2024-09-13 02:34:19 -0400

    remove hero from app.jsx and add to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
+import { Hero } from './components/Hero/Hero';
 import '../../styles/global.scss'
 
 function HomePage() {
 
     return (
-      <>
+      <>     
+       <Hero />
  <img className="static-image" src="../../src/assets/images/telus-1.png"></img>
  <img className="static-image" src="../../src/assets/images/telus-2.png"></img>
  <img className="static-image" src="../../src/assets/images/telus-3.png"></img>
