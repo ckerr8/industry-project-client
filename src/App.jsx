1314440484621ec2e02fb216db0bf8757875535b
Lily Le <lmlt8180@gmail.com>
2024-09-12 20:52:47 -0400

    import hero component, set up browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Hero } from "./components/Hero/Hero";
 import "./styles/global.scss";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Hero />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
