102fe37d6349f768994905a100374b8a5b7832f1
Lily Le <lmlt8180@gmail.com>
2024-09-12 21:48:28 -0400

    delete div containers for images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,19 @@
+import { useCarousel } from "../../hooks/useCarousel";
 import "./Hero.scss";
 
 export function Hero() {
+    const { currentIndex, images } = useCarousel();
+    const { src, alt } = images[currentIndex];
+
     return (
         <section className="hero">
             <div className="hero__container">
                 <div className="hero__overlay"></div>
                 <div className="hero__carousel">
-                    <div className="hero__carousel-items">
-                        <div className="hero__carousel-item item1"></div>
-                        <div className="hero__carousel-item item2"></div>
-                        <div className="hero__carousel-item item3"></div>
-                        <div className="hero__carousel-item item4"></div>
-                        <div className="hero__carousel-item item5"></div>
-                    </div>
+                    <img
+                    className="hero__carousel-item"
+                    src={src}
+                    alt={alt} />
                 </div>
             </div>
         </section>
