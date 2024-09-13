e742b1c74e64474600822488d83b46b50fc56205
Lily Le <lmlt8180@gmail.com>
2024-09-12 22:08:23 -0400

    add header section, import btn, and style both

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import { useCarousel } from "../../hooks/useCarousel";
+import { Button } from "../Button/Button";
 import "./Hero.scss";
 
 export function Hero() {
@@ -9,6 +10,15 @@ export function Hero() {
         <section className="hero">
             <div className="hero__container">
                 <div className="hero__overlay"></div>
+
+                <div className="hero__heading">
+                    <h1 className="hero__header">
+                    Find Your Perfect TV Match And Unlock Your Personalized Channel Bundle!
+                    </h1>
+
+                    <Button className="hero__btn" text="Take the quiz now" />
+                </div>
+
                 <div className="hero__carousel">
                     <img
                     className="hero__carousel-item"
