3e9f5020c7011eca7eca255ecf5fa3a7c3fca008
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-09-12 23:15:31 -0700

    updated hero image to have correct fonts from global

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,7 +21,7 @@ export function Hero() {
                         Unlock Your Personalized Channel Bundle!
                     </h2>
 
-                    <Button className="hero__btn" text="Take the quiz now" />
+                    <Button className="buttons hero__btn" text="TAKE THE QUIZ NOW" />
                 </div>
 
                 <div className="hero__carousel">
