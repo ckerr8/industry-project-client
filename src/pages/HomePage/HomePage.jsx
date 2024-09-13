3a6ee0f992d6f770fe1486b7bde828b9e722b2bf
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-09-13 09:05:41 -0700

    Moved the static images to the app.jsx page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,14 +4,10 @@ import '../../styles/global.scss'
 function HomePage() {
 
     return (
-      <>     
+
        <Hero />
- <img className="static-image" src="../../src/assets/images/telus-1.png"></img>
- <img className="static-image" src="../../src/assets/images/telus-2.png"></img>
- <img className="static-image" src="../../src/assets/images/telus-3.png"></img>
- <img className="static-image" src="../../src/assets/images/telus-4.png"></img>
- <img className="static-image" src="../../src/assets/images/telus-5.png"></img>
-      </>
+
+
     )
   }
   
