376dcbfcdef41d2ecfeb7b69dc47834b1ed8a89a
Lily Le <lmlt8180@gmail.com>
2024-09-12 21:05:20 -0400

    add carousel code and styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,15 @@ export function Hero() {
     return (
         <section className="hero">
             <div className="hero__container">
-                <div className="hero__overlay">
-                    Placeholder Hero
+                <div className="hero__overlay"></div>
+                <div className="hero__carousel">
+                    <div className="hero__carousel-items">
+                        <div className="hero__carousel-item item1"></div>
+                        <div className="hero__carousel-item item2"></div>
+                        <div className="hero__carousel-item item3"></div>
+                        <div className="hero__carousel-item item4"></div>
+                        <div className="hero__carousel-item item5"></div>
+                    </div>
                 </div>
             </div>
         </section>
